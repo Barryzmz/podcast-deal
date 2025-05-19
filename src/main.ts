e0f1201b8f43a05600c7ff8cd96bd9a66fd1f45c
BarryZhuang <1007koi@gmail.com>
2025-05-19 20:40:15 +0800

    feat: 新增element-plus

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,14 @@ import App from './App.vue'
 import router from "./router";
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 
-// createApp(App).mount('#app')
 //創建一個應用
 const app = createApp(App)
 //使用路由器
 app.use(router)
+
+app.use(ElementPlus)
 //掛載整個應用到app容器中
 app.mount('#app')
