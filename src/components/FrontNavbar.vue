fccc952e6b5b32a582f5eae433b2b668158a0b39
BarryZhuang <1007koi@gmail.com>
2025-06-19 20:42:40 +0800

    feat: 調整FrontNavbar，是否登入

--- src/components/FrontNavbar.vue
+++ src/components/FrontNavbar.vue
@@ -1,74 +1,122 @@
 <template>
-  <nav class="navbar navbar-expand-lg navbar-dark" 
-    :class="isScrolled ? 'fixed-top' : ''"
-    style="background-color: #2C2C2C;"
-    >
-    <div class="container-fluid">
-        <div class="col-3">
-          <RouterLink 
-            to="/"
-            class="navbar-brand"
-            :class="isScrolled ? 'text-white' : ''"
-          >
-            <img src="/logo_hands.png" alt="" class="img-fluid d-inline-block align-text-top mx-2" style="height: 30px;">
-            <span class="me-3 d-lg-none">—</span>台灣優惠第一品牌
-          </RouterLink>
-        </div>
-      
-        <div class="col-6 d-flex justify-content-center" >
-          <ul class="navbar-nav ">
-            <RouterLink 
-              to="/about"
-              class="navbar-brand"
-              :class="isScrolled ? 'text-white' : ''"
-              ><span class="me-3 d-lg-none">—</span>關於我們</RouterLink>
-            <RouterLink 
-              to="/podcaster"
-              class="navbar-brand"
-              :class="isScrolled ? 'text-white' : ''"
-            ><span class="me-3 d-lg-none">—</span>Podcaster</RouterLink>
-            <RouterLink 
-              to="/brand"
-              class="navbar-brand"
-              :class="isScrolled ? 'text-white' : ''"
-            ><span class="me-3 d-lg-none">—</span>廣告品牌</RouterLink>
-          </ul>
-        </div>
+  <nav class="navbar navbar-expand-lg navbar-dark" :class="isScrolled ? 'fixed-top' : ''"
+    style="background-color: #2C2C2C;">
+    <div class="container-fluid mx-3">
+      <div class="col-2">
+        <RouterLink to="/" class="navbar-brand" :class="isScrolled ? 'text-white' : ''">
+          <img src="/logo_hands.png" alt="" class="img-fluid d-inline-block align-text-top mx-2" style="height: 30px;">
+          <span class="me-3 d-lg-none">—</span>台灣優惠第一品牌
+        </RouterLink>
+      </div>
 
-        <div class="col-3">
-          <a href="#" class="position-relative me-3">
-            <i class="bi bi-person-circle fs-4 text-white" ></i>
-            <span class="position-absolute top-0 start-100 translate-middle p-1 bg-success border border-light rounded-circle">
-              <span class="visually-hidden">account</span>
-            </span>
-          </a>
-          <a href="#" class="position-relative me-3">
-            <i class="bi bi-heart-fill fs-4 text-white"></i>
-            <span class="position-absolute top-0 start-100 translate-middle p-1 bg-danger border border-light rounded-circle">
-              <span class="visually-hidden">New alerts</span>
-            </span>
-          </a>
-          <a href="#" class="position-relative">
-            <i class="bi bi-cart-fill fs-4 text-white"></i>
-            <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
-              3
-              <span class="visually-hidden">items in cart</span>
-            </span>
-          </a>
+      <div class="col-6 d-flex justify-content-center">
+        <ul class="navbar-nav ">
+          <RouterLink to="/about" class="navbar-brand" :class="isScrolled ? 'text-white' : ''"><span
+              class="me-3 d-lg-none">—</span>關於我們</RouterLink>
+          <RouterLink to="/podcaster" class="navbar-brand" :class="isScrolled ? 'text-white' : ''"><span
+              class="me-3 d-lg-none">—</span>Podcaster</RouterLink>
+          <RouterLink to="/brand" class="navbar-brand" :class="isScrolled ? 'text-white' : ''"><span
+              class="me-3 d-lg-none">—</span>廣告品牌</RouterLink>
+        </ul>
+      </div>
+
+      <div class="col-2 d-flex justify-content-end">
+        <div>
+          <div v-if="!isLoggedIn" class="py-0">
+            <RouterLink to="/login" class="text-decoration-none">
+              <button class="btn btn-outline-light d-flex align-items-center gap-2 rounded-pill px-3 py-0">
+                <i class="bi bi-person fs-5"></i>
+                登入
+              </button>
+            </RouterLink>
+          </div>
+          <div v-else>
+            <div class="d-flex align-items-center gap-3">
+              <RouterLink to="/NewAdvertorial" class="text-decoration-none">
+                <button class="btn btn-outline-light d-flex align-items-center gap-2 rounded-pill px-3 py-0">
+                  <i class="bi bi-plus-lg fs-5 "></i>
+                  建立
+                </button>
+              </RouterLink>
+              
+              <div class="dropdown dropdown">
+                <button class="btn dropdown-toggle p-0" type="button" id="dropdownMenuButton2" data-bs-toggle="dropdown" aria-expanded="false">
+                  <span class="cursor-pointer m-0">
+                    <el-avatar :src="accountProfile.image || circleUrl"  shape="circle" fit="cover" />
+                  </span>
+                </button>
+                <ul class="dropdown-menu dropdown-menu-end dropdown-menu-dark" aria-labelledby="dropdownMenuButton2">
+                  <li>
+                    <RouterLink to="/login" class="dropdown-item">
+                      <div class=" d-flex align-items-center gap-2 pe-4">
+                        <i class="bi bi-person-badge fs-4"></i>
+                        <span>查看個人檔案</span>
+                      </div>
+                    </RouterLink>
+                  </li>
+                  <li>
+                    <div class="dropdown-item d-flex align-items-center gap-2 pe-4">
+                      <i class="bi bi-gear-fill fs-4"></i>
+                      <span>設定</span>
+                    </div>
+                  </li>
+                  <li>
+                    <RouterLink to="/login" class="dropdown-item">
+                      <div class=" d-flex align-items-center gap-2 pe-4">
+                        <i class="bi bi-box-arrow-right fs-4"></i>
+                        <span>登出</span>
+                      </div>
+                    </RouterLink>
+                  </li>
+                </ul>
+              </div>
+
+            </div>
+          </div>
         </div>
       </div>
+    </div>
   </nav>
 </template>
 
 <script setup>
-import { ref, reactive, toRefs, toRef, computed, watch, onMounted, onBeforeUnmount  } from "vue";
-import { RouterView, RouterLink} from 'vue-router';
+import { ref, onMounted, onBeforeUnmount } from "vue";
+import { RouterLink, useRouter } from 'vue-router';
 import avatar1 from '/logo_hands.png'
-
+import commuteforme from '@/assets/image/podcaster/台灣通勤第一品牌.jpg'
+const router = useRouter()
 const isScrolled = ref(false);
 const handleScroll = () => {
   isScrolled.value = window.scrollY > 0;
 };
+const isLoggedIn = ref(true);
+const circleUrl = ref(
+  'https://cube.elemecdn.com/3/7c/3ea6beec64369c2642b92c6726f1epng.png'
+)
+const accountProfile = ref({
+  accountId: 'commuteImg',
+  accountName: '台灣通勤第一品牌',
+  name: '',
+  image: commuteforme,
+  type: '',
+  introduction: ''
+})
+
+function handleCommand(command) {
+  switch (command) {
+    case 'profile':
+      router.push('/profile')
+      break
+    case 'settings':
+      router.push('/settings')
+      break
+    case 'logout':
+      // 清除登入狀態、導向登入頁等
+      isLoggedIn.value = false
+      router.push('/login')
+      break
+  }
+}
 
 onMounted(() => {
   window.addEventListener('scroll', handleScroll);
@@ -78,8 +126,18 @@ onBeforeUnmount(() => {
 });
 </script>
 
-<style>
-.nvbar-bg-init{
+<style scoped>
+.nvbar-bg-init {
   background-color: rgb(234, 225, 211);
 }
+
+/* .cursor-pointer {
+  cursor: pointer;
+} */
+
+/* button:focus {
+  outline: none;
+} */
 </style>
+
+<style></style>
